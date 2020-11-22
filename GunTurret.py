091b1bd85a19ba2d0562a63505fffa74dfe47087
sahith00 <bhsahith@yahoo.com>
2020-11-21 17:41:14 -0800

    Added Turret class and implemented camera_sweep function

--- GunTurret.py
+++ GunTurret.py
@@ -1,43 +1,33 @@
 import numpy as np
 from Entity import Line, Point
 from Geometry import Geometry
+from Turret import Turret
 from utils.coordination_calculator import *
 import sympy as sp
 import math
 import matplotlib.pyplot as plt
 
-class GunTurret(Geometry):
+class GunTurret(Turret):
     def __init__(self, q1_given, q2_given, v0, p0T,
                  p01=np.array([0, -100, 0]).reshape(3, 1),
                  p12=np.array([0, 0, 75]).reshape(3, 1),
                  pOffset=np.array([0, 50, 0]).reshape(3, 1),
                  orig=np.array([100, 50, 0]).reshape(3, 1)):
-        super().__init__()
-        self.q1_given = q1_given
-        self.q2_given = q2_given
+        super().__init__(q1_given, q2_given, pOffset, p12, orig)
         self.v0 = v0
         self.p01 = p01
-        self.p12 = p12
-        self.pOffset = pOffset
-        self.orig = orig
         self.num_q = 2
 
         #  Target
         self.p0T = p0T
 
         # Trajectory calculations at angle
-        self.q1, self.q2, self.t = sp.symbols('q1 q2 t')
+        self.t = sp.symbols('t')
         self.dv = -(1/2) * 9.81 * sp.cos(self.q2) * self.t**2
         self.dh = -(1/2) * 9.81 * sp.sin(self.q2) * self.t**2 + self.v0 * self.t
 
         # Derive forms of homogenous transform T and Jacobian J
         self.T, self.J = self.fwdkin()
-
-    def setQ1(self, q1):
-        self.q1_given = q1
-
-    def setQ2(self, q2):
-        self.q2_given = q2
 
     def setP0T(self, p0T):
         self.p0T = p0T
@@ -182,70 +172,7 @@ class GunTurret(Geometry):
         return -q1_sol, over_sol[0], over_sol[1], (self.p01 + Rzq1*self.p12 + Rzq1*Rxq2*p2T).subs([[self.q1, -q1_sol],
                                                                                                    [self.q2, over_sol[0]]])
 
-    """
-    WIP: S Curve based on position distance instead of angular distance using Jacobian
-    def scurvePath(self, init_pos, dest_pos, coeff_s, t_elapse, time_step):
-        # Find unit vector and norm from init_pos to dest_pos
-        norm = np.linalg.norm(dest_pos - init_pos)
-        u = (dest_pos - init_pos)/norm
-
-        # Determine S-Curve coefficients
-        # S(t) = A/(1 + e^(-k(t - t0)))
-        # A = norm
-        # t0 = 1/2 target time to destination
-        # k = coeff_s <--- TODO
-        t0 = t_elapse/2
-
-        # Find number of time steps and allocate path matrix at size
-        # q x s, where q is the number of joints and s is the number of time steps
-        num_steps = math.ceil(t_elapse/time_step)
-        omega = np.zeroes([self.num_q, num_steps])
-
-        for i in range(num_steps):
-            # Find current distance along unit vector
-            s = norm/(1 + math.e**(-coeff_s((i*time_step) - t0)))
-            s_prime = s(1 - s)
-
-            # Project along unit vector
-            p = s_prime * u
-
-            # Convert to angular velocities with jacobian
-    """
-
-    def scurvePath(self, init_q, dest_q, coeff_s, t_elapse, time_step):
-        # Find unit vector and norm from init_pos to dest_pos
-        #norm = np.linalg.norm(dest_q - init_q)
-        norm = ((dest_q[0, 0] - init_q[0, 0])**2 + (dest_q[1, 0] - init_q[1, 0])**2)**.5
-
-        if norm == 0:
-            return np.array([[0, dest_q[0][0], dest_q[1][0]]]).T
-
-        u = (dest_q - init_q) / norm
-
-        # Determine S-Curve constants
-        # S(t) = A/(1 + e^(-k(t - t0)))
-        # A = norm
-        # t0 = 1/2 target time to destination
-        # k = coeff_s <--- TODO
-        t0 = t_elapse / 2
-
-        # Find number of time steps and allocate path matrix at size
-        # q x s, where q is the number of joints and s is the number of time steps
-        num_steps = math.ceil(t_elapse / time_step)
-        q_steps = np.zeros([self.num_q+1, num_steps])
-
-        for i in range(num_steps):
-            # Find current distance along unit vector
-            s = norm / (1 + math.e ** (-coeff_s * ((i * time_step) - t0)))
-
-            # Project along unit vector and add initial q
-            q_step = s * u + init_q
-
-            q_steps[0, i:i+1] = (i * time_step)
-            q_steps[1:3,i:i+1] = q_step
-
-        return q_steps
-
+# for testing
 if __name__ == "__main__":
     gt = GunTurret(100, [-100, 50, 100])
     gt.inverseKin()
