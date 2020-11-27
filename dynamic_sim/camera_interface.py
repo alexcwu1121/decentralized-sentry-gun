0315ee4be5b789a557af00a56746ee8e690ceef6
unknown <alexcwu1121@gmail.com>
2020-11-27 05:56:02 -0500

    Implement camera interface

---
+++ dynamic_sim/camera_interface.py
@@ -0,0 +1,69 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+from comms import Comms
+import numpy as np
+from Engine import Engine
+from Target import Target
+from CameraTurret import CameraTurret
+import time
+import queue
+#from RobotRaconteur.Client import *
+
+"""
+Grabs frames from camera and runs forward kinematics on detected markers
+"""
+class CameraInterface():
+    def __init__(self):
+        self.name = "camera"
+
+        # Targets defined here for simulation only
+        self.cTurret = CameraTurret(CameraTurret([Target(np.array([0, 100, 100]).reshape(3, 1), "t0")]))
+
+        self.Comms = Comms()
+        self.Comms.add_subscriber_port('127.0.0.1', '3000', 'cState')
+        self.Comms.add_publisher_port('127.0.0.1', '3004', 'targetPos')
+
+    def getCState(self):
+        try:
+            self.cameraPath = self.Comms.get('cState').payload
+        except queue.Empty:
+            pass
+
+    def grabFrame(self):
+        pass
+
+    def publishPos(self, id, target):
+        """
+        Publishes target coordinates and target id in origin frame to pathing algorithms
+        """
+        self.Comms.define_and_send(id, 'targetPos', target)
+
+    def run(self):
+        prev_frame = time.time()
+        # twenty frames per second
+        frame_delay = .05
+
+        while(True):
+            # If a time delay has passed, grab a frame (in simulation, do nothing)
+            if (time.time() - prev_frame >= frame_delay):
+                self.grabFrame()
+                prev_frame = time.time()
+
+            # Update camera turret coords
+            self.getCState()
+
+            # run marker detection on frame (or for simulation, check for targets in range
+            targets = self.cTurret.targetsInView()
+
+            # if marker detected (or target is within view range for simulation), compute forward kinematics.
+            #   for simulation, first derive target in camera frame, then run forward kinematics
+            # on target and publish with topic set to marker id.
+            t_links = self.cTurret.getTargetLinks(p_2, targets)
+            for t_link, target in zip(t_links, targets):
+                p_1 = self.orig + R01 @ self.p12
+                p_2 = p_1 + R01 @ R12 @ self.pOffset
+                targ_pos = self.cTurret.representTarget(self, t_link) + self.cTurret.orig
+                self.publishPos(target.id, targ_pos)
